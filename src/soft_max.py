c85ac22ec2a83b7724c6de4d96160100f8801fa0
yude <yude@lab403-1F.lan>
2017-04-06 19:55:50 +0800

    add short_codes

--- src/soft_max.py
+++ src/soft_max.py
@@ -1,8 +1,7 @@
 """Softmax."""
-
-scores = [3.0, 1.0, 0.2]
-
 import numpy as np
+#scores = [3.0, 1.0, 0.2]
+scores = np.array([[1,2,3,6],[2,4,5,7],[3,9,3,6]])
 
 
 def softmax(x):
@@ -15,7 +14,7 @@ print(softmax(scores))
 import matplotlib.pyplot as plt
 
 x = np.arange(-2.0, 6.0, 0.1)
-scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
-
+scores = np.vstack([x/10, np.ones_like(x)/10, 0.2 * np.ones_like(x)/10])
+print softmax(scores).T
 plt.plot(x, softmax(scores).T, linewidth=2)
 plt.show()
